31c9f2ce87ed6ba236c5500f51c1e07710573c78
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2023-04-13 15:11:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,9 @@
-let 啟動器 = 0
 input.onButtonPressed(Button.A, function () {
-    啟動器 = 1
+    basic.showNumber(input.compassHeading())
 })
 input.onGesture(Gesture.Shake, function () {
-    while (啟動器 == 1) {
-        basic.showNumber(randint(1, 6))
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    啟動器 = 3
-    while (啟動器 == 3) {
-        basic.showNumber(input.compassHeading())
-    }
+    basic.showNumber(randint(1, 6))
 })
 input.onButtonPressed(Button.B, function () {
-    啟動器 = 2
-    while (啟動器 == 2) {
-        basic.showString("[" + input.temperature() + "]")
-    }
+    basic.showString("[" + input.temperature() + "]")
 })
